569a534f277d5713aef0785c463bf6d2fc917bf6
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-04 19:39:19 +0100

    Added radio buttond and refactored the code

--- lib/rps.rb
+++ lib/rps.rb
@@ -5,7 +5,6 @@ class RPS < Sinatra::Base
 
   set :views, Proc.new { File.join(root, '..', "views") }
   enable :sessions
-
   GAME = Game.new
 
   get '/' do
@@ -22,11 +21,13 @@ class RPS < Sinatra::Base
     player = Player.new
     player.name = params[:name]
     GAME.add_player(player)
-    if GAME.has_two_players?
-      session[:current_player] = :player2
-    else
-      session[:current_player] = :player1
-    end
+
+      if GAME.has_two_players?
+        session[:current_player] = :player2
+      else
+        session[:current_player] = :player1
+      end
+
     redirect '/waiting'
   end
 
@@ -43,43 +44,24 @@ class RPS < Sinatra::Base
   get '/result' do
     @option = Option.new(params[:object])
 
-    if session[:current_player] == :player1
-      GAME.player1.pick(@option)
-    else
-      GAME.player2.pick(@option)
-    end
+      if session[:current_player] == :player1
+        GAME.player1.pick(@option)
+      else
+        GAME.player2.pick(@option)
+      end
+
     redirect '/waiting_again'
   end
 
   get '/waiting_again' do
-    redirect '/actual_result' if GAME.ready?
+    redirect '/finale' if GAME.ready?
     erb :waiting_again
   end
 
-  get '/actual_result' do
+  get '/finale' do
     @player = session[:current_player]
-
-    if (@player == :player1 && GAME.winner == :player1) || (@player == :player2 && GAME.winner == :player2)
-      redirect '/win'
-    elsif (@player == :player1 && GAME.winner == :player2) || (@player == :player2 && GAME.winner == :player1)
-      redirect '/lost'
-    else 
-      redirect '/draw'
-    end
+    erb :finale
   end
-
-  get '/win' do
-    "Congratulations!!! You won!!!"
-  end
-
-  get '/lost' do
-    "Sorry you lost. Better luck next time."
-  end
-
-  get '/draw' do
-    "You drew!"
-  end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
